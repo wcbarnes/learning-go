bb7128276154b97ce2c7fb82439dea2eaf4e1aa8
William Barnes <barnesw91291@gmail.com>
2016-11-29 12:58:42 -0800

    added a string util package that has a reverse function

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/wcbarnes/hello/stringutil"
+) 
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
 }
